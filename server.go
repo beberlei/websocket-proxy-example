82f56d7ee4c49d0709a5829c5d021cc5b0caa2ff
Benjamin Eberlei <kontakt@beberlei.de>
2016-04-29 07:59:59 +0200

    Refactored server for simplicity again.

--- server.go
+++ server.go
@@ -10,6 +10,12 @@ import (
 )
 
 var maxId = 0
+var udpAddr string = "127.0.0.1:8081"
+var httpAddr string = ":8080"
+var clients            map[int]*Client
+var clientAdded        chan *Client
+var clientDisconnected chan *Client
+var eventReceived      chan *Event
 
 type Event struct {
 	UserId  int
@@ -24,22 +30,12 @@ func (self *Event) String() string {
 type Client struct {
 	id        int
 	ws        *websocket.Conn
-	server    *Server
 	sendEvent chan *Event
 }
 
-func NewClient(ws *websocket.Conn, s *Server) *Client {
-	maxId++ // this is not thread-safe
-	return &Client{
-		ws:        ws,
-		server:    s,
-		id:        maxId,
-		sendEvent: make(chan *Event, 10),
-	}
-}
-
-func (c *Client) ListenEvents() {
+func (c *Client) Start() {
 	log.Println("Listening to events")
+	clientAdded <- c
 	for {
 		select {
 		case event := <-c.sendEvent:
@@ -47,45 +43,35 @@ func (c *Client) ListenEvents() {
 			err := websocket.JSON.Send(c.ws, event)
 
 			if err != nil {
-				c.server.clientDisconnected <- c
+				clientDisconnected <- c
+				return
 			}
 		}
 	}
 }
 
-// Chat server.
-type Server struct {
-	clients            map[int]*Client
-	clientAdded        chan *Client
-	clientDisconnected chan *Client
-	eventReceived      chan *Event
-}
+func HandleWsRequest(ws *websocket.Conn) {
+	defer ws.Close()
+	maxId++ // this is not thread-safe
 
-func NewServer() *Server {
-	return &Server{
-		clients:            make(map[int]*Client),
-		clientAdded:        make(chan *Client),
-		clientDisconnected: make(chan *Client),
-		eventReceived:      make(chan *Event, 100),
+	client := &Client{
+		id: maxId,
+		ws: ws,
+		sendEvent: make(chan *Event, 10),
 	}
+
+	client.Start()
 }
 
-func (s *Server) ListenHttp() {
-
+func ListenHttp() {
 	log.Println("Listening server...")
 
-	http.Handle("/ws", websocket.Handler(func(ws *websocket.Conn) {
-		defer ws.Close()
-
-		client := NewClient(ws, s)
-		s.clientAdded <- client
-		client.ListenEvents() // blocks
-	}))
+	http.Handle("/ws", websocket.Handler(HandleWsRequest))
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(httpAddr, nil)
 }
 
-func (s *Server) EventLoop() {
+func EventLoop() {
 	timer := time.NewTicker(time.Duration(60) * time.Second).C
 
 	for {
@@ -93,9 +79,9 @@ func (s *Server) EventLoop() {
 
 		case <-timer:
 			log.Printf("Max clients %d\n", maxId)
-			log.Printf("Sending pings to %d to check for timeouts.\n", len(s.clients))
-			if len(s.clients) > 0 {
-				for id, client := range s.clients {
+			log.Printf("Sending pings to %d to check for timeouts.\n", len(clients))
+			if len(clients) > 0 {
+				for id, client := range clients {
 					select {
 					case client.sendEvent <- &Event{UserId: id, Name: "Ping"}:
 					default:
@@ -103,27 +89,27 @@ func (s *Server) EventLoop() {
 				}
 			}
 
-		case event := <-s.eventReceived:
-			if client, ok := s.clients[event.UserId]; ok {
+		case event := <-eventReceived:
+			if client, ok := clients[event.UserId]; ok {
 				select {
 				case client.sendEvent <- event:
 				default:
 				}
 			}
 
-		case c := <-s.clientAdded:
-			s.clients[c.id] = c
-			log.Printf("Added new client %d, now %d clients connected.\n", c.id, len(s.clients))
+		case c := <-clientAdded:
+			clients[c.id] = c
+			log.Printf("Added new client %d, now %d clients connected.\n", c.id, len(clients))
 
-		case c := <-s.clientDisconnected:
+		case c := <-clientDisconnected:
 			log.Println("Delete client")
-			delete(s.clients, c.id)
+			delete(clients, c.id)
 		}
 	}
 }
 
-func (s *Server) ListenUdp() {
-	serverAddr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:8081")
+func ListenUdp() {
+	serverAddr, _ := net.ResolveUDPAddr("udp", udpAddr)
 	conn, _ := net.ListenUDP("udp", serverAddr)
 
 	buf := make([]byte, 1024)
@@ -144,16 +130,20 @@ func (s *Server) ListenUdp() {
 			continue
 		}
 
-		s.eventReceived <- event
+		eventReceived <- event
 	}
 }
 
 func main() {
 	log.SetFlags(log.Lshortfile)
 
-	server := NewServer()
-	go server.ListenHttp()
-	go server.ListenUdp()
+	clients =            make(map[int]*Client)
+	clientAdded =        make(chan *Client)
+	clientDisconnected = make(chan *Client)
+	eventReceived =     make(chan *Event, 100)
 
-	server.EventLoop()
+	go ListenHttp()
+	go ListenUdp()
+
+	EventLoop()
 }
