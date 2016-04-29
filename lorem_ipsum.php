e85e76699cc0bed4f5379f59d59018f25ec78746
Benjamin Eberlei <kontakt@beberlei.de>
2016-04-29 08:08:03 +0200

    Lorem ipsum

---
+++ lorem_ipsum.php
@@ -0,0 +1,21 @@
+<?php
+
+$counterUsers = $argv[1];
+
+$words = explode(" ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus felis, condimentum vitae enim quis, dictum mollis lorem. Sed non dignissim ligula. Quisque luctus porttitor dui, a dapibus diam gravida non. Nulla sit amet hendrerit ligula. Nulla at est rutrum, hendrerit ipsum sed, eleifend dolor. Sed dolor diam, fermentum sit amet tincidunt vel, cursus sed enim. Ut sagittis tellus dolor, in scelerisque velit porttitor id. Morbi quis volutpat erat, et volutpat enim. Donec ut odio imperdiet, pharetra orci eu, lacinia nisi. Nulla quis ullamcorper turpis. Nam commodo orci nec justo faucibus sagittis. Donec nec ante sollicitudin, finibus mi eu, blandit felis. Cras eget purus sem. Curabitur pretium tempor lacinia.");
+$colors = ["green", "red", "blue", "pink", "yellow", "orange", "black", "cyan"];
+$count = 0;
+
+while (true) {
+    $fp = @stream_socket_client("udp://127.0.0.1:8081", $errno, $errstr, 1);
+    for ($userId = 1; $userId <= $counterUsers; $userId++) {
+        $message = $words[$count % count($words)];
+        $color = $colors[$count % count($colors)];
+
+        fwrite($fp, json_encode(['UserId' => $userId, 'Name' => 'Message', 'Payload' => ['text' => $message]]));
+        fwrite($fp, json_encode(['UserId' => $userId, 'Name' => 'Background', 'Payload' => ['color' => $color]]));
+    }
+    fclose($fp);
+    sleep(1);
+    $count++;
+}
